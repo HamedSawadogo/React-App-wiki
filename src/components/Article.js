cb1d7b250c29da84b684e67923df887986b48d49
Sawadogo <kerb418@gmail.com>
2023-03-15 13:39:41 +0100

    version 2 du projet

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,27 @@
-import React, { useEffect, useState } from "react";
-import "../styles/article.css";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+// import useContext from "react";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
+const ArticleWrapper = styled.div`
+  background: ${({ color }) => color};
+  padding: 10px;
+  border-radius: 5px;
+  width: 90%;
+  height: auto;
+  display: block;
+  margin: 10px auto;
+  border: 1px solid whitesmoke;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+`;
 const Article = ({ article }) => {
   const [edit, setEdit] = useState(false);
   const [editValue, setEditValue] = useState("");
   const [time, setTime] = useState(0);
 
+  const { theme } = useContext(ThemeContext);
   setInterval(() => {
     const CalculateCurentMinutes = (date) => {
       const date1 = new Date(date);
@@ -27,7 +42,7 @@ const Article = ({ article }) => {
     });
   };
   return (
-    <div className="article">
+    <ArticleWrapper color={theme}>
       <h3>{article.author}</h3>
       <p>{article.content}</p>
       <small>publié le {formatDate(article.date)}</small>
@@ -52,7 +67,7 @@ const Article = ({ article }) => {
         />
       )}
       {editValue && <p>{editValue}</p>}
-    </div>
+    </ArticleWrapper>
   );
 };
 
--- src/components/User.js
+++ src/components/User.js
@@ -1,24 +1,48 @@
 import React from "react";
-import "../styles/user.css";
 import { motion } from "framer-motion";
+import styled from "styled-components";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 
+const UserWrapper = styled.div`
+  width: 90%;
+  height: 250px;
+  padding: 10px;
+  background: ${({ color }) => color};
+  margin: 10px;
+  text-align: center;
+  border-radius: 20px;
+  transition: 0.3s;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin: 0 auto;
+`;
 const User = ({ user }) => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <motion.div
-      animate={{ x: [30, 100, 1] }}
-      transition={{ ease: "easeOut", duration: 2 }}
-      className="user"
-    >
-      <img src={user.picture.large} alt="" />
-      <h3>
-        {user.name.first}
-        {user.name.last}
-      </h3>
-      <p>habite à {user.location.city}</p>
-      <small> devellopeur dépuis {user.registered.age}ans</small>
-      <br />
-      <small>{user.registered.age > 10 ? "❤️" : "❤️❤️"}</small>
-    </motion.div>
+    <UserWrapper color={theme}>
+      <motion.div
+        animate={{ x: [30, 100, 1] }}
+        transition={{ ease: "easeOut", duration: 2 }}
+        className="user"
+      >
+        <Image src={user.picture.large} alt="" />
+        <h3>
+          {user.name.first}
+          {user.name.last}
+        </h3>
+        <p>habite à {user.location.city}</p>
+        <small> devellopeur dépuis {user.registered.age}ans</small>
+        <br />
+        <small>{user.registered.age > 10 ? "❤️" : "❤️❤️"}</small>
+      </motion.div>
+    </UserWrapper>
   );
 };
 
