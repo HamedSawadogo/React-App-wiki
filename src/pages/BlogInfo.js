fe964465cc69a2043d0e3bd919ae159e359c2d21
Sawadogo <kerb418@gmail.com>
2023-03-15 09:59:30 +0100

    gestion des routes

--- src/pages/BlogInfo.js
+++ src/pages/BlogInfo.js
@@ -1,26 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import {useParams } from 'react-router-dom';
-import Navigation from '../components/Navigation';
-import axios from 'axios';
-import Article from '../components/Article';
+import React, { useEffect, useState } from "react";
+import { Navigate, redirect, useNavigate, useParams } from "react-router-dom";
+import Navigation from "../components/Navigation";
+import axios from "axios";
+import Article from "../components/Article";
 
 const BlogInfo = () => {
-   const {id}=useParams();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-   const [article,setArticle]=useState([]);
-   useEffect(()=>{
-      if(id){
-        axios.get(`http://localhost:3004/articles/${id}`)
-        .then((res)=>setArticle(res.data));
-      }
-   },[]);
-   return (
-      <div>
-         <Navigation/>
-         <h1 style={{textAlign:"center"}}>Bienvenu sur l'article {id}</h1>
-         <Article article={article} />
-      </div>
-   );
+  const [article, setArticle] = useState([]);
+  useEffect(() => {
+    if (isNaN(id)) {
+      navigate("/error", { replace: true });
+    }
+    try {
+      axios.get(`http://localhost:3004/articles/${id}`).then((res) => {
+        setArticle(res.data);
+        console.log(res);
+      });
+    } catch (error) {
+      navigate("/error", { replace: true });
+    }
+  }, []);
+  return (
+    <div>
+      <Navigation />
+      <h1 style={{ textAlign: "center" }}>Bienvenu sur l'article {id}</h1>
+      <Article article={article} />
+    </div>
+  );
 };
 
 export default BlogInfo;
