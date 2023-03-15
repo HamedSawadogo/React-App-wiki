3c88eecdeed5541f080b3c8267c70734b7c6bbce
Sawadogo <kerb418@gmail.com>
2023-03-15 06:25:01 +0100

    about des coeurs

--- src/components/User.js
+++ src/components/User.js
@@ -4,25 +4,12 @@ import "../styles/user.css";
 const User = ({user}) => {
    return (
       <div className='user'>
-         <img src={
-               user.picture.large
-            }
-            alt=""/>
-         <h3>{
-            user.name.first
-         }
-            {
-            user.name.last
-         }</h3>
-         <p>habite à {
-            user.location.city
-         }</p>
-         <small>
-            devellopeur dépuis {
-            user.registered.age
-         }
-            ans</small>
-
+         <img src={user.picture.large} alt=""/>
+         <h3>{user.name.first}{user.name.last}</h3>
+         <p>habite à {user.location.city}</p>
+         <small> devellopeur dépuis {user.registered.age}ans</small>
+         <br/>
+         <small>{user.registered.age>10?'❤️':'❤️❤️'}</small>
       </div>
    );
 };
